192b1e574a721f4f6dec0b023b938727dd96ac4a
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-11-23 16:11:10 +0900

    Add mobile navigation without styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
+const menuBtn = document.querySelector(".menu__btn");
+const navigationContainer = document.querySelector(".navigation");
 
+const media = window.matchMedia("(min-width: 768px)");
+
+menuBtn.addEventListener("click", function toggleActiveClass() {
+  navigationContainer.classList.toggle("navigation--active");
+});
+
+media.addEventListener("change", function removeActiveClass(event) {
+  if (event.matches) {
+    navigationContainer.classList.remove("navigation--active");
+  }
+});
+
+const resizeObserver = new ResizeObserver(function addResizingClass() {
+  document.body.classList.add("resizing");
+
+  requestAnimationFrame(function removeResizingClass() {
+    document.body.classList.remove("resizing");
+  });
+});
+
+resizeObserver.observe(document.body);
