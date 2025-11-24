e190246cb8a17b0fa5fa423809f7829b885c6211
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2025-11-24 10:46:27 +0900

    Add active menu state for navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 const menuBtn = document.querySelector(".menu__btn");
 const navigationContainer = document.querySelector(".navigation");
-
+const navigationListItems = document.querySelectorAll(".navigation__item");
 const media = window.matchMedia("(min-width: 768px)");
+
+[...navigationListItems].forEach((item) => {
+  item.addEventListener("click", () => {
+    document
+      .querySelector(".navigation__item--active")
+      ?.classList.remove("navigation__item--active");
+    item.classList.add("navigation__item--active");
+  });
+});
 
 menuBtn.addEventListener("click", function toggleActiveClass() {
   navigationContainer.classList.toggle("navigation--active");
